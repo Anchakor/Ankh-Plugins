c1cd74376204f351d170887b4a74f141ca1e1336
Jiří Procházka <ojirio@gmail.com>
2011-09-18 14:24:54 +0200

    putting distortion dsp to separate class

--- ankh-distortion.cpp
+++ ankh-distortion.cpp
@@ -1,19 +1,7 @@
 /*
-  LV2 Amp Example Plugin
-  Copyright 2006-2011 Steve Harris <steve@plugin.org.uk>,
+  LV2 Ankh Plugins
+  Copyright 2006-2011 Jiří Procházka <ojirio@gmail.com>,
                       David Robillard <d@drobilla.net>
-
-  Permission to use, copy, modify, and/or distribute this software for any
-  purpose with or without fee is hereby granted, provided that the above
-  copyright notice and this permission notice appear in all copies.
-
-  THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
 #include <math.h>
@@ -21,6 +9,7 @@
 #include <string.h>
 
 #include "LV2Plugin.hpp"
+#include "distortion.cpp"
 
 
 #define ANKHPLUGIN ANKHDistortion
@@ -31,74 +20,32 @@
 
 class ANKHPLUGIN : public LV2::Plugin<ANKHPLUGIN> {
 private:
-    float toutl, toutr;
-    float samplerate, oldsoftclip, sclip;
     float hpfOldInL, hpfOldOutL, hpfOldInR, hpfOldOutR;
+    Ankh::Distortion *distl, *distr;
 
 protected:
-    float hardclip(float in) {
-        if(in > 1.0) {
-            return 1.0;
-        } else if(in < -1.0) {
-            return -1.0;
-        } else
-            return in;
-    }
-
-    float hpf(float in, float& oldin, float& oldout) {
-        // y(n) = x(n) - x(n-1) + R * y(n-1) 
-        // (-3dB @ 30Hz): R = 1-(190/samplerate) // R = 1 - (pi*2 * frequency /samplerate) (pi=3.14159265358979)
-        
-        float R = 1 - (3.0 / samplerate);
-        float out = (in + R * oldin) - oldin;   //float out = in - oldin + R * oldout;
-        oldin = in + R * oldin;                 //oldin = in; oldout = out;
-        return out;
-    }
-
-    void ankhprocess(float *inl, float *inr, float *outl, float *outr, 
-            float *mix, float *dcoffset, float *softclip, float *hardclipgain) {
-        toutl = *inl;
-        toutr = *inr;
-
-        // soft clipping
-        if(oldsoftclip != *softclip) {
-            sclip = pow(*softclip * 30, 2.0);
-            oldsoftclip = *softclip;
-        }
-        if(sclip > 1e-30) {
-            toutl = (1.0 / atan(sclip)) * atan(*dcoffset + (toutl * sclip));
-            toutr = (1.0 / atan(sclip)) * atan(*dcoffset + (toutr * sclip));
-        }
-
-        // 1 pole HPF cutting < 1Hz to fix the dc offset
-        toutl = hpf(toutl, hpfOldInL, hpfOldOutL);
-        toutr = hpf(toutr, hpfOldInR, hpfOldOutR);
-
-        // gain
-        toutl = (1 + *hardclipgain * 30) * toutl;
-        toutr = (1 + *hardclipgain * 30) * toutr;
-
-        // hard clipping
-        toutl = hardclip(toutl);
-        toutr = hardclip(toutr);
-
-        // mixing
-        *outl = (*mix * toutl) + ((1 - *mix) * *inl);
-        *outr = (*mix * toutr) + ((1 - *mix) * *inr);
-    }
 
 public:
     ANKHPLUGIN(double sample_rate, const char*, const LV2::Feature* const*) : LV2::Plugin<ANKHPLUGIN>(NPARAMETERS+NINSOUTS) {
-        samplerate = sample_rate;
         hpfOldInL = 0.0; hpfOldOutL = 0.0;
+        distl = new Ankh::Distortion(sample_rate, &hpfOldInL, &hpfOldOutL);
         hpfOldInR = 0.0; hpfOldOutR = 0.0;
+        distl = new Ankh::Distortion(sample_rate, &hpfOldInR, &hpfOldOutR);
+    }
+
+    ~ANKHPLUGIN() {
+        free(distl); free(distr);
     }
     
     void run(uint32_t sample_count) {
         for(uint32_t sample=0; sample < sample_count; sample++){
-            ankhprocess(p<float>(0)+sample, 
-                    p<float>(1)+sample, 
+            distl->process(p<float>(0)+sample, 
                     p<float>(2)+sample, 
+                    p<float>(4), 
+                    p<float>(5), 
+                    p<float>(6), 
+                    p<float>(7));
+            distl->process(p<float>(1)+sample, 
                     p<float>(3)+sample, 
                     p<float>(4), 
                     p<float>(5), 
---
+++ distortion.cpp
@@ -0,0 +1,70 @@
+/*
+  LV2 Ankh Plugins
+  Copyright 2011 Jiří Procházka <ojirio@gmail.com>
+*/
+
+#include <math.h>
+#include <stdlib.h>
+
+namespace Ankh {
+
+class Distortion {
+private:
+    float tout;
+    float samplerate, oldsoftclip, sclip;
+    float *hpfOldIn, *hpfOldOut;
+
+public:
+    Distortion(double sample_rate, float *hpfOldIn_, float *hpfOldOut_) {
+        samplerate = sample_rate;
+        hpfOldIn = hpfOldIn_;
+        hpfOldOut = hpfOldOut_;
+    }
+
+    float hardclip(float in) {
+        if(in > 1.0) {
+            return 1.0;
+        } else if(in < -1.0) {
+            return -1.0;
+        } else
+            return in;
+    }
+
+    float hpf(float in, float& oldin, float& oldout) {
+        // y(n) = x(n) - x(n-1) + R * y(n-1) 
+        // (-3dB @ 30Hz): R = 1-(190/samplerate) // R = 1 - (pi*2 * frequency /samplerate) (pi=3.14159265358979)
+        
+        float R = 1 - (3.0 / samplerate);
+        float out = (in + R * oldin) - oldin;   //float out = in - oldin + R * oldout;
+        oldin = in + R * oldin;                 //oldin = in; oldout = out;
+        return out;
+    }
+
+    void process(float *in, float *out, float *mix, float *dcoffset, float *softclip, float *hardclipgain) {
+        tout = *in;
+
+        // soft clipping
+        if(oldsoftclip != *softclip) {
+            sclip = pow(*softclip * 30, 2.0);
+            oldsoftclip = *softclip;
+        }
+        if(sclip > 1e-30) {
+            tout = (1.0 / atan(sclip)) * atan(*dcoffset + (tout * sclip));
+        }
+
+        // 1 pole HPF cutting < 1Hz to fix the dc offset
+        tout = hpf(tout, *hpfOldIn, *hpfOldOut);
+
+        // gain
+        tout = (1 + *hardclipgain * 30) * tout;
+
+        // hard clipping
+        tout = hardclip(tout);
+
+        // mixing
+        *out = (*mix * tout) + ((1 - *mix) * *in);
+    }
+
+};
+
+}
